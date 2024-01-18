808105a011a0239fc31baf81bbb75a60d5074d9c
Lobsang <lobsangmercedes@gmail.com>
2024-01-17 23:19:25 -0400

    Start of project, go slowly but cosistence

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className="bg-red-100">
+
+      Hola
+     </div>
+    </>
+  )
+}
+
+export default App
